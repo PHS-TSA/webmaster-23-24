bdb99e1bfe36060f1aac7abd3da2a901f9d08c9f
Eli <88557639+lishaduck@users.noreply.github.com>
2023-12-14 09:34:23 -0600

    feat: actual homepage, sorta

--- routes/index.tsx
+++ routes/index.tsx
@@ -13,10 +13,10 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Why Switch?</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          Looking for information about solar power? You've come to the right
+          place!
         </p>
         <Counter count={count} />
       </div>
