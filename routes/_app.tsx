a5a5f63d2bdc13f1565d50daddb5ec88c65b12bf
Eli <88557639+lishaduck@users.noreply.github.com>
2023-12-14 13:07:42 -0600

    fix: wrapper page title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>webmaster-23-24</title>
+        <title>Why Switch?</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
