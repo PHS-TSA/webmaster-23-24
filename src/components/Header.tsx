3bd4896863ab2a13b79ec4ae0414d6141415dcc7
Eli <88557639+lishaduck@users.noreply.github.com>
2023-12-15 15:26:34 -0600

    fix(web): breakpoint
    Previously, it was breaking too soon.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header({ active }: Props) {
   ];
 
   return (
-    <div class="bg-white w-full py-6 px-8 flex flex-col md:flex-row gap-4">
+    <div class="bg-white w-full py-6 px-8 flex flex-col sm:flex-row gap-4">
       <div class="flex items-center flex-1">
         <IconSolarPanel2 aria-hidden="true" />
         <div class="text-2xl ml-1 font-bold">
