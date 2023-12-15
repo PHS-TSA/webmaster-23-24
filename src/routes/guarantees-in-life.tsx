e2754f1a5cd5dedc04b98cb0062756a58bf6d8e5
Eli <88557639+lishaduck@users.noreply.github.com>
2023-12-15 15:37:10 -0600

    feat(web): page name
    Add in a page name for the head.
    Necessary to test csr.

--- src/routes/guarantees-in-life.tsx
+++ src/routes/guarantees-in-life.tsx
@@ -1,21 +1,29 @@
+import { Head } from "$fresh/runtime.ts";
 import { Logo } from "../components/Logo.tsx";
 
 export default function Taxes() {
   return (
-    <div class="px-4 py-8 mx-auto bg-green-400">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <Logo />
-        <h1 class="text-4xl font-bold">Death. And Taxes.</h1>
-        <p class="my-4">
-          Looking for information about tax rebates and incentives for green
-          energy?
-          <br />
-          Please keep in mind that, like death, taxes are a certainty, and thus
-          tax evasion is illegal. If you wish to avoid taxes, please consult a
-          registered accountant rather than willy-nilly skipping them based on
-          our advice.
-        </p>
+    <>
+      <Head>
+        <title>
+          Ben!
+        </title>
+      </Head>
+      <div class="px-4 py-8 mx-auto bg-green-400">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <Logo />
+          <h1 class="text-4xl font-bold">Death. And Taxes.</h1>
+          <p class="my-4">
+            Looking for information about tax rebates and incentives for green
+            energy?
+            <br />
+            Please keep in mind that, like death, taxes are a certainty, and
+            thus tax evasion is illegal. If you wish to avoid taxes, please
+            consult a registered accountant rather than willy-nilly skipping
+            them based on our advice.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
