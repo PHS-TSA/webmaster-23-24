ba64483fc9a11d7621b8596a4e766dc31362f593
Eli <88557639+lishaduck@users.noreply.github.com>
2023-12-16 11:31:37 -0600

    feat(tooling): first test
    It's mostly useless, but it feels good!*
    
    See: I Got You (I Feel Good)

---
+++ src/routes/about.test.ts
@@ -0,0 +1,21 @@
+import { createHandler, ServeHandlerInfo } from "$fresh/server.ts";
+import manifest from "../fresh.gen.ts";
+import config from "../fresh.config.ts";
+import { assertStringIncludes } from "$std/assert/mod.ts";
+
+const CONN_INFO: ServeHandlerInfo = {
+  remoteAddr: { hostname: "127.0.0.1", port: 53496, transport: "tcp" },
+};
+
+Deno.test("HTTP assert test.", async (t) => {
+  const handler = await createHandler(manifest, config);
+
+  await t.step("#1 GET /about/", async () => {
+    const resp = await handler(
+      new Request("http://127.0.0.1/about/"),
+      CONN_INFO,
+    );
+    const text = await resp.text();
+    assertStringIncludes(text, "It's us, man!");
+  });
+});
