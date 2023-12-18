cb6198c9d047afdb14dfa9a1504dd85cdf6cccd5
Eli <88557639+lishaduck@users.noreply.github.com>
2023-12-18 08:14:41 -0600

    feat: the footer doesn't look awful on mobile (part 3)
    A few changes from part 2 to make it superb.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,7 +48,7 @@ const Footer: FunctionalComponent = () => {
   ];
 
   return (
-    <div class="bg-white dark:bg-black grid grid-flow-col grid-rows-footer-mobile grid-cols-footer-mobile sm:grid-rows-footer-desktop sm:grid-cols-footer-desktop w-full max-w-screen-xlg gap-16 sm:gap-8 md:gap-16 px-8 py-8 text-sm">
+    <div class="bg-white dark:bg-black grid grid-flow-col grid-rows-footer-mobile grid-cols-footer-mobile sm:grid-rows-footer-desktop sm:grid-cols-footer-desktop w-full max-w-screen-xlg gap-x-2 gap-y-16 sm:gap-x-8 md:gap-16 p-8 text-sm">
       <div class="row-start-1 row-end-2 col-start-1 col-end-3 sm:col-end-2">
         <div class="flex items-center gap-1">
           <IconSolarPanel2
@@ -68,7 +68,7 @@ const Footer: FunctionalComponent = () => {
           key={item.title}
         >
           <a
-            class="font-bold dark:text-white p-4"
+            class="font-bold dark:text-white py-4 pr-4"
             href={item.url}
           >
             {item.title}
@@ -77,7 +77,7 @@ const Footer: FunctionalComponent = () => {
             {item.children.map((child) => (
               <li class="mt-2" key={child.name}>
                 <a
-                  class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 p-4"
+                  class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 py-4 pr-4"
                   href={item.url + child.href}
                 >
                   {child.name}
@@ -88,7 +88,7 @@ const Footer: FunctionalComponent = () => {
         </div>
       ))}
 
-      <div class="text-gray-500 dark:text-gray-400 space-y-2 align-middle col-start-3 col-end-4 sm:col-start-auto sm:col-end-auto">
+      <div class="text-gray-500 dark:text-gray-400 space-y-2 align-middle col-start-3 col-end-4 sm:col-start-auto sm:col-end-auto row-start-1 row-end-3 sm:row-end-auto">
         <div class="text-xs m-1">
           Made with
         </div>
