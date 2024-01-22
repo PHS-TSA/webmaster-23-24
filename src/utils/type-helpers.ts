e7fbe585620d2029132afedee3b65031ab4223d1
Eli <88557639+lishaduck@users.noreply.github.com>
2024-01-22 11:55:52 -0600

    style: stricter type

--- src/utils/type-helpers.ts
+++ src/utils/type-helpers.ts
@@ -4,7 +4,7 @@ export function hasItems(menu: Menu): menu is MenuWithItems {
   return (menu.items?.length ?? 0) > 0;
 }
 
-export function isKey<T extends object>(
+export function isKey<const T extends object>(
   obj: T,
   key: PropertyKey,
 ): key is keyof T {
