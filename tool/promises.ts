08d2892ffa02b738e26eac7e4d0fefff8c84be41
Eli <88557639+lishaduck@users.noreply.github.com>
2024-01-23 20:29:48 -0600

    style: loosen type requirements

--- tool/promises.ts
+++ tool/promises.ts
@@ -1,6 +1,16 @@
+/**
+ * Maps each element of an iterable or async iterable to a new value using the provided function.
+ *
+ * @template T The type of the elements in the iterable.
+ * @template U The type of the resulting values after mapping.
+ * @param iterable The iterable or async iterable to map.
+ * @param func The mapping function to apply to each element.
+ * @param run The optional predicate function to determine if an element should be mapped.
+ * @returns A promise that resolves to an array of the mapped values.
+ */
 export async function map<T, U>(
-  iterable: Iterable<T> | AsyncIterable<T>,
-  func: (iter: T) => Promise<U>,
+  iterable: AsyncIterable<T> | Iterable<T>,
+  func: (iter: T) => Promise<U> | U,
   run: (iter: T) => boolean = (_) => true,
 ): Promise<U[]> {
   const promises = [];
