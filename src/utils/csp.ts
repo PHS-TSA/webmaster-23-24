808c36672d28a89ccabe7129bd0c9a0766565148
Eli <88557639+lishaduck@users.noreply.github.com>
2024-03-16 12:26:33 -0500

    fix: csp

--- src/utils/csp.ts
+++ src/utils/csp.ts
@@ -3,13 +3,17 @@ import { SELF, UNSAFE_INLINE, useCSP } from "$fresh/runtime.ts";
 export function useCsp(): void {
   useCSP((csp) => {
     csp.directives.scriptSrcElem ??= [];
+    csp.directives.styleSrc ??= [];
     csp.directives.styleSrcElem ??= [];
     csp.directives.imgSrc ??= [];
+    csp.directives.connectSrc ??= [];
     csp.directives.manifestSrc ??= [];
 
     csp.directives.scriptSrcElem.push(SELF, UNSAFE_INLINE);
+    csp.directives.styleSrc.push(SELF);
     csp.directives.styleSrcElem.push(SELF);
     csp.directives.imgSrc.push(SELF);
+    csp.directives.connectSrc.push(SELF);
     csp.directives.manifestSrc.push(SELF);
   });
 }
