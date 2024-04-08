4eae25622aaa06b1ada948abde1c130b6f7c645a
Eli <88557639+lishaduck@users.noreply.github.com>
2024-04-08 16:41:37 -0500

    chore: less code
    54 characters removed, for a total of a 50 line reduction of code!

--- src/islands/Chatbot.tsx
+++ src/islands/Chatbot.tsx
@@ -108,7 +108,7 @@ function ChatbotBox(props: JSX.HTMLAttributes<HTMLDivElement>): JSX.Element {
             key={msg.message}
             class={`${getReplySide(msg.role)} ${replyStyles}`}
             // biome-ignore lint/security/noDangerouslySetInnerHtml: It's back!
-            dangerouslySetInnerHTML={{ __html: render(msg.message, {}) }}
+            dangerouslySetInnerHTML={{ __html: render(msg.message) }}
           />
         ))}
       </div>
