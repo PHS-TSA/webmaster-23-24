837c0df6475a6aba13daa54c4f354e63871c30cf
Eli <88557639+lishaduck@users.noreply.github.com>
2024-05-05 16:58:54 -0500

    fix: strict csp
    The csp didn't work right with latest fresh.

--- src/utils/csp.ts
+++ src/utils/csp.ts
@@ -1,7 +1,8 @@
-import { SELF, UNSAFE_INLINE, useCSP } from "$fresh/runtime.ts";
+import { SELF, STRICT_DYNAMIC, UNSAFE_INLINE, useCSP } from "$fresh/runtime.ts";
 
 export function useCsp(): void {
   useCSP((csp) => {
+    csp.directives.scriptSrc ??= [];
     csp.directives.scriptSrcElem ??= [];
     csp.directives.styleSrc ??= [];
     csp.directives.styleSrcElem ??= [];
@@ -9,9 +10,10 @@ export function useCsp(): void {
     csp.directives.connectSrc ??= [];
     csp.directives.manifestSrc ??= [];
 
+    csp.directives.scriptSrc.push(STRICT_DYNAMIC);
     csp.directives.scriptSrcElem.push(SELF, UNSAFE_INLINE);
     csp.directives.styleSrc.push(SELF);
-    csp.directives.styleSrcElem.push(SELF, UNSAFE_INLINE);
+    csp.directives.styleSrcElem.push(SELF, STRICT_DYNAMIC);
     csp.directives.imgSrc.push(SELF, "data:");
     csp.directives.connectSrc.push(SELF);
     csp.directives.manifestSrc.push(SELF);
