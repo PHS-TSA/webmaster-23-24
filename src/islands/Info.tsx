a06c37fd817d03fe1bafdc82eaffe83da1c8c390
Eli <88557639+lishaduck@users.noreply.github.com>
2024-05-16 10:51:37 -0500

    fix: remove outline from info tooltip buttons

--- src/islands/Info.tsx
+++ src/islands/Info.tsx
@@ -9,12 +9,16 @@ export interface InfoProps {
 export function Info({ children }: InfoProps): JSX.Element {
   return (
     <Popover as={Fragment}>
-      <PopoverButton type="button" title="More Information">
+      <PopoverButton
+        type="button"
+        title="More Information"
+        class="focus-visible:outline-none"
+      >
         <IconInfoCircle class="inline" size={20} />
       </PopoverButton>
       <PopoverPanel
         anchor="bottom"
-        class="absolute left-0 z-30 top-0 max-w-fit w-48 border-4 bg-slate-900/80 p-1 text-sm prose prose-sm prose-invert shadow-2xl border-none rounded-md"
+        class="absolute left-0 z-20 top-0 max-w-fit w-48 border-4 bg-slate-900/80 p-1 text-sm prose prose-sm prose-invert shadow-2xl border-none rounded-md"
       >
         {children}
       </PopoverPanel>
