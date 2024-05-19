423221883c24a5ecb7cb6e10f7554a52d576e5ad
Eli <88557639+lishaduck@users.noreply.github.com>
2024-05-18 19:52:10 -0500

    perf: cache logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,19 @@
+import { asset } from "$fresh/runtime.ts";
 import { clsx } from "clsx";
 import type { JSX } from "preact";
 import { logoAlt } from "../site.ts";
 
 /**
- * Render the logo.
- * Currently, this is just a lightning bolt.
- * Eventually, it will be our logo.
+ * Render our logo.
  *
+ * @param props - The component's properties.
  * @returns The rendered logo component.
  */
 export function Logo(props: JSX.HTMLAttributes<HTMLImageElement>): JSX.Element {
   return (
     <img
       class={clsx("my-6", props.class)}
-      src="/logo.svg"
+      src={asset("/logo.svg")}
       width={500}
       height={500}
       alt={logoAlt}
