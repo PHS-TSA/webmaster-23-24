c50b13fcebb057eb88d0d0244d02f832a940fc00
Eli <88557639+lishaduck@users.noreply.github.com>
2024-05-22 13:13:54 -0500

    fix: dynamically generate carousel styles

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { asset } from "$fresh/runtime.ts";
 import type { JSX } from "preact";
 import { ScrollDown } from "../islands/ScrollDown.tsx";
+import { css } from "../utils/tags.ts";
 import type { HeroProps } from "./Cover.tsx";
 
 export interface CarouselProps extends HeroProps {
@@ -13,8 +14,61 @@ export function Carousel({
   heros,
   scrollDown,
 }: CarouselProps): JSX.Element {
+  const totalAnimationTime = heros.length * 5;
+  const visibilityPercentage = 100 / (2 * heros.length);
+
+  const keyframes = css`
+    0% {
+      opacity: 0;
+    }
+    ${visibilityPercentage}%,
+    ${2 * visibilityPercentage}% {
+      opacity: 1;
+    }
+    ${3 * visibilityPercentage}%,
+    100% {
+      opacity: 0;
+    }
+`;
+
+  const delays = heros
+    .map((_, i) => {
+      return css`
+        &:nth-child(${i + 1}) {
+          animation-delay: ${
+            (i * 2 * visibilityPercentage * totalAnimationTime) / 100
+          }s;
+        }
+    `;
+    })
+    .join("\n");
+
+  const styles = css`
+    .carousel > img {
+      opacity: 0;
+      animation: animate-fade ${totalAnimationTime}s infinite;
+    }
+
+    .carousel.hero > img {
+      animation: image-down linear both, animate-fade ${totalAnimationTime}s infinite;
+      animation-timeline: --page-scroll, auto;
+      animation-range: 0 100svh, normal;
+    }
+
+    .carousel > img {
+      ${delays}
+    }
+
+    @keyframes animate-fade {
+      ${keyframes}
+    }
+  `;
+
   return (
     <div class="relative flex h-[65svh] flex-col px-4 pt-2 sm:pt-3 md:h-[75svh] md:pt-4 lg:h-svh lg:pt-24">
+      {/* biome-ignore lint/security/noDangerouslySetInnerHtml: It's just CSS that we control. */}
+      <style dangerouslySetInnerHTML={{ __html: styles }} />
+
       <div class="carousel hero">
         {heros.map((hero) => (
           <img key={hero} src={asset(hero)} alt="" />
